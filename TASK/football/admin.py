676558f7cdbf2893352d6f7643b06d73ff003ac9
KeziaSJ <78840620+KeziaSJ@users.noreply.github.com>
2021-08-26 12:48:36 +0530

    Delete TASK directory

--- TASK/football/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import player
-
-
-admin.site.register(player)
--- TASK/football/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms  
-from .models import player 
-
-class playerform(forms.ModelForm):  
-    class Meta:  
-        model = player  
-        fields = "__all__"  
